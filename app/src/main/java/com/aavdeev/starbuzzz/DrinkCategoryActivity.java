3cdb417fec3c26b427b2a34744dc220d26fb2566
avdey <avdey87@gmail.com>
2017-03-14 17:56:02 +0300

    285. DrinkCategoryActivity

--- app/src/main/java/com/aavdeev/starbuzzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/aavdeev/starbuzzz/DrinkCategoryActivity.java
@@ -1,6 +1,20 @@
 package com.aavdeev.starbuzzz;
 
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-class DrinkCategoryActivity {
+public class DrinkCategoryActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listDrinks = getListView();
+        ArrayAdapter<Drink> listAdpter = new ArrayAdapter<Drink>(
+                this, android.R.layout.simple_list_item_1, Drink.drinks
+        );
+        listDrinks.setAdapter(listAdpter);
+    }
 }
